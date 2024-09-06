151837ae3d62fd9373636589cde7f1bb7cf0c601
권대호 <kown9512@gmail.com>
2024-09-06 15:24:18 +0900

    [WE] init: gitignore

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+// https://khj0426.tistory.com/238
+// https://vitejs.dev/guide/env-and-mode
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
