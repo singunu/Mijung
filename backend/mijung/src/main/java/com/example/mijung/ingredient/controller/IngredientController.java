ba652594e8d45a43286f6b1f25546bec077841b2
jiwon <idjjm95@gmail.com>
2024-09-19 13:45:20 +0900

    [BE] feat: 스프링 환경 설정

---
+++ backend/mijung/src/main/java/com/example/mijung/ingredient/controller/IngredientController.java
@@ -0,0 +1,10 @@
+package com.example.mijung.ingredient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ingredients")
+public class IngredientController {
+
+}
