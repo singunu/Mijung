e24b43b44eeeb5bb29f3873acaec308d5926f441
김봉상 <tph02361@gmail.com>
2024-09-19 13:58:24 +0900

    [FE] feat: tailwindCSS 설정, Navbar UI 초안 추가 #S11P21D107-266

--- frontend/src/widgets/Navbar/Navbar.tsx
+++ frontend/src/widgets/Navbar/Navbar.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-xl font-bold">This is Navbar</h1>
+    <nav className="bg-blue-600 shadow-lg">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-7">
+            <div>
+              <Link to="/" className="flex items-center py-4 px-2">
+                <span className="font-semibold text-white text-lg">로고</span>
+              </Link>
+            </div>
+          </div>
+          <div className="hidden md:flex items-center space-x-1">
+            <Link
+              to="/ingredients"
+              className="py-4 px-2 text-white hover:text-blue-200 transition duration-300"
+            >
+              식재료 찾기
+            </Link>
+            <Link
+              to="/recipes"
+              className="py-4 px-2 text-white hover:text-blue-200 transition duration-300"
+            >
+              레시피 찾기
+            </Link>
+            <Link
+              to="/recipes/jjim"
+              className="py-4 px-2 text-white hover:text-blue-200 transition duration-300"
+            >
+              찜한 레시피
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
