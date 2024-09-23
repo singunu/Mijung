cdff08a521a116880cfd3ae4f07601b48e3de316
김소연 <xhwkrlxhdi@naver.com>
2024-09-23 10:31:51 +0900

    [BE] feat: RestControllerAdvice를 통한 예외 처리 추가 #S11P21D107-288

---
+++ backend/mijung/src/main/java/com/example/mijung/error/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.mijung.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    // 400 Bad Request: (잘못된 요청입니다)
+    // 404 Not Found: (정보를 찾을 수 없습니다)
+    // 500 Internal Server Error: (서버 에러 입니다)
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException e) {
+        log.error("Response Status Error : " + e.getMessage());
+        return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Method Argument Not Valid Error : " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        log.error("서버 에러 발생 : " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
