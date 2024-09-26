81e4bab5043148d47ab888639b906e4b3be27793
김봉상 <tph02361@gmail.com>
2024-09-26 10:57:01 +0900

    [FE] feat: IngredientDetail API 재설계 #S11P21D107-311

---
+++ frontend/src/pages/IngredientDetailPage/IngredientDetailAPI.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_BASE_URL =
+  window.location.hostname === 'localhost'
+    ? `${import.meta.env.VITE_APP_DEV_URL_SPRING}:8080/api/v1` // 개발 환경의 API 주소
+    : `${import.meta.env.VITE_APP_PRODUCT_URL}/api/v1`; // 배포 환경의 API 주소
+
+export interface IngredientInfo {
+  ingredientId: number;
+  name: string;
+  retailUnit: string;
+  retailUnitsize: string;
+  image: string;
+  price: string;
+  changeRate: number;
+  changePrice: number;
+}
+
+export const getIngredientInfo = async (
+  ingredientId: number
+): Promise<IngredientInfo> => {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/ingredients/${ingredientId}/info`
+    );
+    console.log(API_BASE_URL);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('식재료 정보 조회 API 오류:', error);
+    throw error;
+  }
+};
