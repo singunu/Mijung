9a293d10ec19362ceface07e40874fd9d34300f4
박태우 <myxodn12@naver.com>
2024-09-27 14:36:12 +0900

    feat: 레시피 조회 기능 추가 #S11P21D107-365

--- backend/mijung/src/main/java/com/example/mijung/recipe/repository/RecipeRepository.java
+++ backend/mijung/src/main/java/com/example/mijung/recipe/repository/RecipeRepository.java
@@ -1,10 +1,12 @@
 package com.example.mijung.recipe.repository;
 
 import com.example.mijung.recipe.entity.Recipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
-
+    Page<Recipe> findByNameContaining(String keyword, Pageable pageable);
 }
