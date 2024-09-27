41573bb2700e1d183e63c9654654738e1a2a1086
권대호 <kown9512@gmail.com>
2024-09-27 16:39:24 +0900

    [FE] chore: app router 절대경로 적용 #S11P21D107-393

--- frontend/src/app/AppRouter.tsx
+++ frontend/src/app/AppRouter.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
-import MainPage from '../pages/MainPage/MainPage';
-import IngredientListPage from '../pages/IngredientListPage/IngredientList';
-import IngredientDetailPage from '../pages/IngredientDetailPage/IngredientDetail';
-import { RecipeDetailPage } from '../pages/RecipeDetailPage/RecipeDetail';
-import RecipeJjimPage from '../pages/RecipeJjimPage/RecipeJjimDetail';
+import MainPage from '@/pages/MainPage/MainPage';
+import IngredientListPage from '@/pages/IngredientListPage/IngredientList';
+import IngredientDetailPage from '@/pages/IngredientDetailPage/IngredientDetail';
+import { RecipeDetailPage } from '@/pages/RecipeDetailPage/RecipeDetail';
+import RecipeJjimPage from '@/pages/RecipeJjimPage/RecipeJjimDetail';
 import Layout from './RoutingLayout/Layout';
-import { Error } from '../shared/components';
-import { RecipeListPage } from '../pages/RecipeListPage';
+import { Error } from '@/shared/components';
+import { RecipeListPage } from '@/pages/RecipeListPage';
 
 export const router = createBrowserRouter([
   {
