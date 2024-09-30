0d79ed9b3142eb68943a576a14aa7d014ac2571d
권대호 <kown9512@gmail.com>
2024-09-30 10:26:29 +0900

    [FE] chore: 주석 제거 #S11P21D107-411

--- frontend/src/pages/RecipeListPage/index.tsx
+++ frontend/src/pages/RecipeListPage/index.tsx
@@ -1,6 +1,5 @@
 import SideLayout from '@/app/RoutingLayout/SideLayout';
 import MainLayout from '@/app/RoutingLayout/MainLayout';
-// import Searchbar from '@/widgets/Searchbar/Searchbar';
 import { RecipeList } from '@/features/recipeList/ui/RecipeList';
 import { RecipeSearchBar } from '@/features/recipeList/ui/RecipeSearchBar';
 
@@ -9,23 +8,8 @@ export const RecipeListPage = () => {
     <div className="grid grid-cols-10">
       <SideLayout />
       <MainLayout>
-        {/* <Searchbar type="recipes" /> */}
-        {/* <div className="container mx-auto px-4 py-8">
-          <h1 className="text-3xl font-bold mb-6">레시피 목록</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {sampleRecipes.map((recipe) => (
-              <div
-                key={recipe.id}
-                className="bg-white shadow-md rounded-lg p-6"
-              >
-                <h2 className="text-xl font-semibold mb-2">{recipe.name}</h2>
-                <p className="text-gray-600">{recipe.ingredients}</p>
-              </div>
-            ))}
-          </div>
-        </div> */}
-        <RecipeList />
         <RecipeSearchBar />
+        <RecipeList />
       </MainLayout>
       <SideLayout />
     </div>
