a85fe8ef56f437036d9c1617c928798a92cb1e11
vaaast-lake <kown9512@gmail.com>
2024-10-02 07:34:20 +0900

    [FE] feat: 레시피 리스트 페이지네이션 구현 #S11P21D107

--- frontend/src/features/recipeList/ui/RecipeList.tsx
+++ frontend/src/features/recipeList/ui/RecipeList.tsx
@@ -1,14 +1,34 @@
-import { Error } from '@/shared/components';
+import { queryClient } from '@/shared/query/query-client';
 import { useRecipeList } from '../api/useRecipeList';
+import { useEffect, useState } from 'react';
+import { Error } from '@/shared/components';
+import { recipeApi } from '@/entities/recipe/model/recipeApi';
 
-export const RecipeList = () => {
-  const { data, isLoading, error } = useRecipeList();
+interface Props {
+  keyword: string;
+}
 
-  if (isLoading) return <div>Loding...</div>;
-  if (error) return <Error />;
+export const RecipeList = ({ keyword }: Props) => {
+  const [page, setPage] = useState<number>(0);
+  const { data, isFetching, error } = useRecipeList({ page, keyword });
+
+  useEffect(() => {
+    if (data?.pagination) {
+      const nextPage = (page + 1) % data.pagination.perPage;
+      queryClient.prefetchQuery({
+        queryKey: ['recipe-list', nextPage, data.pagination.perPage, keyword],
+        queryFn: () =>
+          recipeApi.getRecipes(nextPage, data.pagination.perPage, keyword),
+      });
+    }
+  }, [data, page, keyword]);
+
   if (!data) {
-    throw new TypeError('Data is undefined in RecipeList');
+    console.log('No data on RecipeList');
+    return <Error />;
   }
+
+  if (error) return <Error />;
 
   const { recipes, pagination } = data;
 
@@ -26,6 +46,14 @@ export const RecipeList = () => {
           총 {pagination.total} 중 {pagination.page} 페이지
         </span>
       </div>
+      {isFetching ? <span>Loading...</span> : null}
+      <button
+        onClick={() => {
+          setPage((prevPage) => (prevPage + 1) % pagination.perPage);
+        }}
+      >
+        Next Page
+      </button>
     </>
   );
 };
--- frontend/src/pages/RecipeListPage/index.tsx
+++ frontend/src/pages/RecipeListPage/index.tsx
@@ -2,14 +2,23 @@ import SideLayout from '@/app/RoutingLayout/SideLayout';
 import MainLayout from '@/app/RoutingLayout/MainLayout';
 import { RecipeList } from '@/features/recipeList/ui/RecipeList';
 import { RecipeSearchBar } from '@/features/recipeList/ui/RecipeSearchBar';
+import { useState } from 'react';
 
 export const RecipeListPage = () => {
+  const [keyword, setKeyword] = useState<string>('');
+  const handleKeywordChange = (inputWord: string) => {
+    setKeyword(inputWord);
+  };
+
   return (
     <div className="grid grid-cols-10">
       <SideLayout />
       <MainLayout>
-        <RecipeSearchBar />
-        <RecipeList />
+        <RecipeSearchBar
+          keyword={keyword}
+          onKeywordChange={handleKeywordChange}
+        />
+        <RecipeList keyword={keyword} />
       </MainLayout>
       <SideLayout />
     </div>
