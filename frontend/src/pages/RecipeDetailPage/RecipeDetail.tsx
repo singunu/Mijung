03ae2bf7ba64a15ee57335e0b124bedebdcfa212
권대호 <kown9512@gmail.com>
2024-10-02 21:28:18 +0900

    [FE] fix: detail page 이동 시 로직 오류 수정 #S11P21D107-443

--- frontend/src/pages/RecipeDetailPage/RecipeDetail.tsx
+++ frontend/src/pages/RecipeDetailPage/RecipeDetail.tsx
@@ -11,39 +11,36 @@ export const RecipeDetailPage = () => {
 
   if (error) return <Error />;
   if (isLoading) return <div>Loading...</div>;
-  if (!recipe) {
-    console.log('Recipe data in RecipeDetailPage is undefined.');
-
-    return <Error />;
-  }
 
   return (
     <div className="grid grid-cols-10">
       <LeftSideLayout />
       <MainLayout>
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-3xl font-bold mb-6">{recipe.name}</h1>
-          <div className="bg-white shadow-md rounded-lg p-6">
-            <h2 className="text-2xl font-semibold mb-4">재료</h2>
-            <ul className="list-disc list-inside text-gray-700 mb-6">
-              {recipe.materials?.map((material, index) => (
-                <li key={index}>{material.name}</li>
-              ))}
-            </ul>
+        {recipe && (
+          <div className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold mb-6">{recipe.name}</h1>
+            <div className="bg-white shadow-md rounded-lg p-6">
+              <h2 className="text-2xl font-semibold mb-4">재료</h2>
+              <ul className="list-disc list-inside text-gray-700 mb-6">
+                {recipe.materials?.map((material, index) => (
+                  <li key={index}>{material.name}</li>
+                ))}
+              </ul>
 
-            <h2 className="text-2xl font-semibold mb-4">조리 방법</h2>
-            <ol className="list-decimal list-inside text-gray-700 mb-6">
-              {recipe.steps?.map((step, index) => (
-                <li key={index}>{step.content}</li>
-              ))}
-            </ol>
+              <h2 className="text-2xl font-semibold mb-4">조리 방법</h2>
+              <ol className="list-decimal list-inside text-gray-700 mb-6">
+                {recipe.steps?.map((step, index) => (
+                  <li key={index}>{step.content}</li>
+                ))}
+              </ol>
 
-            <div className="flex justify-between text-gray-600">
-              <p>조리 시간: {recipe.time}</p>
-              <p>난이도: {recipe.level}</p>
+              <div className="flex justify-between text-gray-600">
+                <p>조리 시간: {recipe.time}</p>
+                <p>난이도: {recipe.level}</p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </MainLayout>
       <RightSideLayout />
     </div>
