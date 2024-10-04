7da36a9580f691bde2350b529525ab4df7084d77
김봉상 <tph02361@gmail.com>
2024-10-04 13:46:33 +0900

    [FE] chore: FE개발자 간 API 테스트 환경 분리 #S11P21D107-404

--- frontend/src/entities/ingredient/model/ingredientApi.ts
+++ frontend/src/entities/ingredient/model/ingredientApi.ts
@@ -2,10 +2,18 @@ import IngredientApi from '../api/ingredient';
 import FakeIngredientClient from '../../../shared/api/fakeIngredientClient';
 import IngredientClient from '../../../shared/api/ingredientClient';
 
-// 환경에 따라 적절한 클라이언트를 선택하여 IngredientApi 인스턴스 생성
-const client =
-  window.location.hostname === 'localhost'
-    ? new FakeIngredientClient()
-    : new IngredientClient();
+// localhost이고, Chrome을 쓰지 않을 때만 fakeAPI로 테스트가능
+const client = (() => {
+  if (window.location.hostname === 'localhost') {
+    if (
+      navigator.userAgent.includes('Chrome') &&
+      !navigator.userAgent.includes('Edg')
+    ) {
+      return new IngredientClient();
+    }
+    return new FakeIngredientClient();
+  }
+  return new IngredientClient();
+})();
 
 export const ingredientApi = new IngredientApi(client);
--- frontend/src/entities/tasteSuggest/model/tasteSuggestApi.ts
+++ frontend/src/entities/tasteSuggest/model/tasteSuggestApi.ts
@@ -2,9 +2,18 @@ import TasteSuggestApi from '../api/tasteSuggest';
 import FakeTasteSuggestClient from '@/shared/api/fakeTasteSuggestClient';
 import TasteSuggestClient from '@/shared/api/tasteSuggestClient';
 
-const client =
-  window.location.hostname != 'localhost'
-    ? new FakeTasteSuggestClient()
-    : new TasteSuggestClient();
+// localhost이고, Chrome을 쓰지 않을 때만 fakeAPI로 테스트가능
+const client = (() => {
+  if (window.location.hostname === 'localhost') {
+    if (
+      navigator.userAgent.includes('Chrome') &&
+      !navigator.userAgent.includes('Edg')
+    ) {
+      return new TasteSuggestClient();
+    }
+    return new FakeTasteSuggestClient();
+  }
+  return new TasteSuggestClient();
+})();
 
 export const tasteSuggestApi = new TasteSuggestApi(client);
