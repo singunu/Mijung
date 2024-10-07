b32f36e9314315fcb8d1ead555aa5a4cb161c4e4
김소연 <xhwkrlxhdi@naver.com>
2024-10-07 17:46:26 +0900

    [BE] feat: 식재료 추천 반환값에 image추가 #S11P21D107-468

--- backend/mijung/src/main/java/com/example/mijung/cart/dto/RecommendIngredientListResponse.java
+++ backend/mijung/src/main/java/com/example/mijung/cart/dto/RecommendIngredientListResponse.java
@@ -13,11 +13,14 @@ public class RecommendIngredientListResponse {
     private final Integer ingredientId;
     @Schema(description = "식재료명", example = "감자")
     private final String name;
+    @Schema(description = "이미지 링크", example = "https://recipe1.ezmember.co.kr/cache/bbs/2016/08/25/e1ecc151809f90a265886b51e9a44bd9.jpg")
+    private final String image;
 
     public static RecommendIngredientListResponse from(Ingredient ingredient) {
         return RecommendIngredientListResponse.builder()
                 .ingredientId(ingredient.getId())
                 .name(ingredient.getItemName())
+                .image(ingredient.getImage())
                 .build();
     }
 }
