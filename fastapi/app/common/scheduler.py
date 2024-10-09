d0bdf107a851e65f5d07edd933530b9d96e14c5f
jiwon <idjjm95@gmail.com>
2024-10-09 23:16:50 +0900

    [BE] fix: scheduler 정보 수정 #S11P21D107-461

--- fastapi/app/common/scheduler.py
+++ fastapi/app/common/scheduler.py
@@ -136,7 +136,7 @@ def start_scheduler():
     scheduler = BackgroundScheduler()
     scheduler.add_job(
         fetch_data_from_api,
-        CronTrigger(hour=22, minute=00, second=0, timezone=pytz.timezone('Asia/Seoul')) #실제 서비스
+        CronTrigger(hour=13, minute=00, second=0, timezone=pytz.timezone('Asia/Seoul')) #실제 서비스
         #CronTrigger(minute='*') #테스트
     )
     scheduler.start()
